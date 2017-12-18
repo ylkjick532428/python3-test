9013dd34b903c65de2409fcd2b0ea6d1abcabc9e
yang yuehui <ylkjick532428@gmail.com>
2017-12-18 22:45:14 +0800

    update

--- app/test6.py
+++ app/test6.py
@@ -55,3 +55,32 @@ exitsingle = 1
 for t in threads:
     t.join()
 print ("stop enter..")
+
+
+# start ..
+# Starting queue list-1
+# list-1 check ticket 100001
+# Starting queue list-2
+# list-2 check ticket 100002
+# Starting queue list-3
+# list-3 check ticket 100003
+# list-2 check ticket 100004
+# list-1 check ticket 100005
+# list-3 check ticket 100006
+# list-2 check ticket 100007
+# list-1 check ticket 100008
+# list-3 check ticket 100009
+# list-2 check ticket 100010
+# list-1 check ticket 100011
+# list-3 check ticket 100012
+# list-2 check ticket 100013
+# list-1 check ticket 100014
+# list-3 check ticket 100015
+# list-1 check ticket 100016
+# list-2 check ticket 100017
+# list-3 check ticket 100018
+# list-2 check ticket 100019
+# close  list-3
+# close  list-1
+# close  list-2
+# stop enter..
